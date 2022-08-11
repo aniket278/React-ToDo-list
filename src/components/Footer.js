850bba1da8388fcf50f71340dc4acda99fe65264
aniket728 <aniketjain7891@gmail.com>
2022-08-11 23:48:50 +0530

    first

---
+++ src/components/Footer.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const style = {
+  backgroundColor: "#F8F8F8",
+  borderTop: "1px solid #E7E7E7",
+  textAlign: "center",
+  padding: "20px",
+  position: "fixed",
+  left: "0",
+  bottom: "0",
+  height: "60px",
+  width: "100%"
+};
+
+export default function App() {
+  return (
+    <div>
+      <div style={style}><i>Copyright @007, Created By Aniket,  Panipat, Haryana</i></div>
+    </div>
+  );
+}
